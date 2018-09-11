05014741e320f875393005e594b15d72a816959e
Paulo McNally <paulomcnally@gmail.com>
2018-09-10 23:05:26 -0600

    Fix #1 - Remove cookie and redirect logic

--- src/components/pages/AccountCreate.js
+++ src/components/pages/AccountCreate.js
@@ -1,4 +1,5 @@
-import React from 'react';
+/* globals window */
+import React, { PureComponent } from 'react';
 import {
   Grid,
   Container,
@@ -7,39 +8,60 @@ import {
 import { Provider } from 'react-redux';
 import Cdn from 'react-cf-helper-cdn';
 import I18n from 'react-cf-helper-i18n';
+import { Cookies } from 'react-cf-graphql-client';
+import is from 'is_js';
 import CardContainer from '../layouts/CardContainer';
 import { Body } from '../../styles';
 import store from '../../store';
 
-const AccountCreate = function AccountCreate() {
-  return (
-    <Provider store={store}>
-      <Body>
-        <Grid centered columns={1}>
-          <Grid.Column
-            computer="six"
-            widescreen="six"
-            tablet="ten"
-            mobile="sixteen"
-            largeScreen="six"
-          >
-            <Container textAlign="center">
-              <h1>
-                {I18n.t('account.create.header')}
-              </h1>
-              <Image
-                src={Cdn.image('logo-text-296-40.svg')}
-                width={296}
-                height={40}
-                centered
-              />
-            </Container>
-            <CardContainer />
-          </Grid.Column>
-        </Grid>
-      </Body>
-    </Provider>
-  );
+class AccountCreate extends PureComponent {
+  componentDidMount() {
+    const tenant = Cookies.get('cf-tenant');
+    const token = Cookies.get('cf-token');
+
+    // check if session exists
+    if (!is.undefined(token)) {
+      // redirect to home
+      window.location = '/';
+    } else if (!is.undefined(tenant)) {
+      // remove cf-tenant cookie
+      Cookies.remove('cf-tenant');
+      
+      // reload page
+      window.location.reload();
+    }
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <Body>
+          <Grid centered columns={1}>
+            <Grid.Column
+              computer="six"
+              widescreen="six"
+              tablet="ten"
+              mobile="sixteen"
+              largeScreen="six"
+            >
+              <Container textAlign="center">
+                <h1>
+                  {I18n.t('account.create.header')}
+                </h1>
+                <Image
+                  src={Cdn.image('logo-text-296-40.svg')}
+                  width={296}
+                  height={40}
+                  centered
+                />
+              </Container>
+              <CardContainer />
+            </Grid.Column>
+          </Grid>
+        </Body>
+      </Provider>
+    );
+  }
 };
 
 export default AccountCreate;
